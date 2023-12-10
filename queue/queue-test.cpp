6bc0bd29237031873bbec68cc9ccbe6f9b69763a
adamreaksmey <adaminiature@gmail.com>
2023-12-10 21:41:21 +0700

    initial

---
+++ queue/queue-test.cpp
@@ -0,0 +1,27 @@
+// Queue implementation in C++
+
+#include <iostream>
+// We define a variable called " SIZE "
+// Setting its value to 5
+#define SIZE 5
+using namespace std;
+
+class Queue {
+    // we then define an array with the size of 5
+    // using the SIZE we defined.
+   private: int items[SIZE], front, rear;
+
+   // defining a basic Queue function with front and rear based on its definition.
+   public: Queue() {
+    front = -1;
+    rear = -1;
+  }
+
+    // we define another function that would check a condition
+    bool isFull() {
+    if (front == 0 && rear == SIZE - 1) {
+      return true;
+    }
+    return false;
+  }
+}
