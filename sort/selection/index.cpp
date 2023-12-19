60ad1a8f744ab1e0e8fa41c58831c45842ecc5bb
adamreaksmey <adaminiature@gmail.com>
2023-12-19 12:24:31 +0700

    forgot swap function

--- sort/selection/index.cpp
+++ sort/selection/index.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
 void selectionSort(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i++)
