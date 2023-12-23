503ef3d5f562b51f3fb1218fa1c4e8cad2a045ed
adamreaksmey <adaminiature@gmail.com>
2023-12-23 07:45:48 +0700

    update

--- sort/quick/performance.cpp
+++ sort/quick/performance.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
+#include <cstdlib>
+#include <algorithm>
 using namespace std;
 
 void swap(int &a, int &b)
@@ -33,30 +34,50 @@ void quickSort(int arr[], int low, int high)
     if (low < high)
     {
         int pi = partition(arr, low, high);
+
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    const int SIZE = 1000000;
-    int arr[SIZE];
+    const int ARRAY_SIZE = 100000; // Size of the array for the performance test
 
-    // Populate the array with random values
+    int arr[ARRAY_SIZE];
     srand(time(0));
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < ARRAY_SIZE; ++i)
     {
-        arr[i] = rand();
+        arr[i] = rand() % 10000;
     }
 
-    // Measure the execution time
-    clock_t start = clock();
-    quickSort(arr, 0, SIZE - 1);
-    clock_t end = clock();
-    double elapsedSeconds = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Original Array: ";
+    printArray(arr, ARRAY_SIZE);
 
-    cout << "Sorting " << SIZE << " elements took " << elapsedSeconds << " seconds." << endl;
+    // Measure the start time for quicksort
+    clock_t quickSortStartTime = clock();
+
+    quickSort(arr, 0, ARRAY_SIZE - 1);
+
+    // Measure the end time for quicksort
+    clock_t quickSortEndTime = clock();
+
+    cout << "Sorted Array using Quicksort: ";
+    printArray(arr, ARRAY_SIZE);
+
+    // Calculate the elapsed time for quicksort in seconds
+    double quickSortElapsedTime = double(quickSortEndTime - quickSortStartTime) / CLOCKS_PER_SEC;
+
+    cout << "Elapsed Time: " << quickSortElapsedTime << " seconds" << endl;
 
     return 0;
 }
